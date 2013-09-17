524c33dcc8561bcd26790328c1c165b42d6a3261
Wei Li <wei.li@feedhenry.com>
2013-09-17 14:52:31 +0100

    update init route

--- client/app/scripts/init.js
+++ client/app/scripts/init.js
@@ -1,7 +1,7 @@
 /*global App, Backbone*/
 var initialize = function(){
   App.routers.mainRoute = new App.Router.MainRoute();
-  Backbone.history.start({pushState: true});
+  Backbone.history.start({pushState: true, root: document.location.pathname});
 };
 
 //check if cordova is available
