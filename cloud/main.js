bb6cec9b4ed4ac1c037705e946a7538c7a034134
jason.madigan@feedhenry.com <jason.madigan@feedhenry.com>
2013-10-02 15:06:11 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,18 +1,25 @@
 var getWeather = require('./weather').getWeather;
 var saveData = require('./databrowser').saveData;
 
-exports.hello = function(params, callback){
+exports.hello = function(params, callback) {
   var hostSpecificResponse = "Hello from the FeedHenry Cloud App named " + process.env.FH_APPNAME;
-  return callback(null, {text: hostSpecificResponse});
+  return callback(null, {
+    text: hostSpecificResponse
+  });
 };
 
-exports.getWeather = function(params, callback){
+exports.getWeather = function(params, callback) {
   return getWeather(params, callback);
 };
 
-exports.saveData = function(params, callback){
+exports.saveData = function(params, callback) {
   return saveData(params, callback);
 };
 
-
-
+exports.listActions = function(params, callback) {
+  return callback(null, {
+    actions: [{
+      yolks: "things"
+    }]
+  });
+}
